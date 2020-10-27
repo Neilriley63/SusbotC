f8e13062d391d932ec46882c2ac6343f92def8be
neil <neilyo613@gmail.com>
2020-10-27 16:48:53 -0400

    Created InfoModule, updates to SusModule and ConjureModule. I honestly don't know what I changed since I didn't commit this last night and haven't touched it in like 16 hours.

---
+++ SusbotC/Modules/InfoModule.cs
@@ -0,0 +1,21 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace SusbotC.Modules
+{
+    public class InfoModule : ModuleBase<SocketCommandContext>
+    {
+
+        string info = "Hi! I'm the Susbot. I am written in C# by Neil Riley. My source code can be found at: https://github.com/Neilriley63/SusbotC. \nMy available commands are: !sus and !conjure.";
+
+        // ~say hello world -> hello world
+        [Command("susinfo")]
+        [Summary("Reply with information about Susbot")]
+        public Task SayAsync()
+            => ReplyAsync(info);
+
+
+        
+    }
+
+}
